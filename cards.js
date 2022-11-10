7791f14552569e1f76332506e1628c1eb78641f3
Andrew Qu <qual1337@gmail.com>
2022-11-10 09:43:01 -0800

    hover cards test

---
+++ cards.js
@@ -0,0 +1,21 @@
+document.getElementById("cards").onmousemove = (e) => {
+  for (const card of document.getElementsByClassName("card-container")) {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+};
+
+document.getElementById("cards").ontouchmove = (e) => {
+  for (const card of document.getElementsByClassName("card-container")) {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+};
